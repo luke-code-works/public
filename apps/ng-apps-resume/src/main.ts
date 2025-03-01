af8ac224b2e0b3a056ef3c4cc63a144197a5dff1
Lukas Voland <lukas.voland@live.com>
2025-03-01 13:25:26 +0100

    refactor: reorganize and change usage of root component

--- apps/ng-apps-resume/src/main.ts
+++ apps/ng-apps-resume/src/main.ts
@@ -1,5 +1,5 @@
 import {bootstrapApplication} from '@angular/platform-browser';
-import {AppComponent} from './app/app.component';
 import {appConfig} from './app/app.config';
+import {RootComponent} from './app/root/root.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(RootComponent, appConfig).catch((err) => console.error(err));
