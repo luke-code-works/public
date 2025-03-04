2ab04fa8122bc902786ae9fa897445e03e283717
Lukas Voland <lukas.voland@live.com>
2025-03-04 22:27:46 +0100

    feat: improve root component and styling

--- apps/ng-apps-resume/src/app/root/root.component.ts
+++ apps/ng-apps-resume/src/app/root/root.component.ts
@@ -1,10 +1,17 @@
 import {Component} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {NxWelcomeComponent} from '../nx-welcome/nx-welcome.component';
+import {RouterOutlet} from '@angular/router';
+import {FooterContainerComponent} from '../ui/footer-container/footer-container.component';
+import {NavContainerComponent} from '../ui/nav-container/nav-container.component';
+import {provideNavigation} from '../ui/nav-container/provider';
+import {NavFragmentsComponent} from '../ui/nav-fragments/nav-fragments.component';
+import {withFragmentNavigation} from '../ui/nav-fragments/provider';
+import {NavTitleComponent} from '../ui/nav-title/nav-title.component';
 
 @Component({
     selector: 'app-root,body[app-root]',
     templateUrl: './root.component.html',
-    imports: [NxWelcomeComponent, RouterModule],
+    styleUrl: './root.component.scss',
+    providers: [provideNavigation(withFragmentNavigation())],
+    imports: [NavContainerComponent, NavTitleComponent, NavFragmentsComponent, RouterOutlet, FooterContainerComponent],
 })
 export class RootComponent {}
