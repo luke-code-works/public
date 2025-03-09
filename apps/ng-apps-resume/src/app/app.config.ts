a197757dca4f273833c041aefd1f0582434258bf
Lukas Voland <lukas.voland@live.com>
2025-03-09 01:13:29 +0100

    revert: "feature/make-angular-zoneless (#10)"
    This reverts commit c7483994dc20ce7409b8cf5f9f8af87003de28c5 due material depending on change detection.

--- apps/ng-apps-resume/src/app/app.config.ts
+++ apps/ng-apps-resume/src/app/app.config.ts
@@ -1,10 +1,10 @@
-import {ApplicationConfig, provideExperimentalZonelessChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter, withInMemoryScrolling} from '@angular/router';
 import {appRoutes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideExperimentalZonelessChangeDetection(),
+        provideZoneChangeDetection({eventCoalescing: true}),
         provideRouter(
             appRoutes,
             withInMemoryScrolling({anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled'}),
