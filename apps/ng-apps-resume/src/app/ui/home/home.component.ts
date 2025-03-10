11970972ba06000f5eb16fe79e0a3156bf3263e5
Lukas Voland <lukas.voland@langevolknant.de>
2025-03-10 09:56:41 +0100

    feat: add some initial content

--- apps/ng-apps-resume/src/app/ui/home/home.component.ts
+++ apps/ng-apps-resume/src/app/ui/home/home.component.ts
@@ -1,9 +1,11 @@
 import {Component, inject} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 import {NavFragmentConfigService} from '../nav-fragments/nav-fragment-config.service';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
+    imports: [MatIconModule],
 })
 export class HomeComponent {
     constructor() {
