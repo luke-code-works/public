81d8a15fdaec9020b729890bfdfbca73b786150f
Luke <133242630+luke-code-works@users.noreply.github.com>
2025-04-02 23:48:27 +0200

    feature/obfuscate-text-a-little (#26)
    feat: obfuscate text a little more

--- apps/ng-apps-resume/src/app/ui/privacy-policy/privacy-policy.component.ts
+++ apps/ng-apps-resume/src/app/ui/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
+import {TextObfuscationComponent} from '../text-obfuscation/text-obfuscation.component';
 import {XorCipherPipe} from '../xor-cipher/xor-cipher.pipe';
 
 @Component({
     selector: 'app-privacy-policy',
     templateUrl: './privacy-policy.component.html',
     styleUrl: './privacy-policy.component.scss',
-    imports: [XorCipherPipe],
+    imports: [XorCipherPipe, TextObfuscationComponent],
 })
 export class PrivacyPolicyComponent {}
