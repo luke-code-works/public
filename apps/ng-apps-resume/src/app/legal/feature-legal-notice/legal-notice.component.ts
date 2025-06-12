94aaf9d25505826e3b1d911ba2e42509ab12c422
Luke <133242630+luke-code-works@users.noreply.github.com>
2025-06-12 09:57:02 +0200

    refactor: implement heading component everywhere

--- apps/ng-apps-resume/src/app/legal/feature-legal-notice/legal-notice.component.ts
+++ apps/ng-apps-resume/src/app/legal/feature-legal-notice/legal-notice.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {provideTranslocoScope, TranslocoDirective} from '@jsverse/transloco';
+import {HeadingWithDotComponent} from '../../shared/ui/heading-with-dot/heading-with-dot.component';
 import {TextObfuscationComponent} from '../../shared/ui/text-obfuscation/text-obfuscation.component';
 import {createTranslocoInlineLoader} from '../../shared/util/transloco/transloco-inline-loader-factory';
 import {XorCipherPipe} from '../../shared/util/xor-cipher/xor-cipher.pipe';
@@ -14,7 +15,7 @@ const legalNoticeTranslocoScope = {
     templateUrl: './legal-notice.component.html',
     styleUrl: './legal-notice.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [TranslocoDirective, XorCipherPipe, TextObfuscationComponent],
+    imports: [TranslocoDirective, HeadingWithDotComponent, XorCipherPipe, TextObfuscationComponent],
     providers: [provideTranslocoScope(legalNoticeTranslocoScope)],
 })
 export class LegalNoticeComponent {}
