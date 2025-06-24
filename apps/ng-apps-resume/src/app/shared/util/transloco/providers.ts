fc3634911c229d5f4f5f87d46ebac6e22012e2c0
Lukas Voland <lukas.voland@live.com>
2025-06-24 11:29:08 +0200

    wip

--- apps/ng-apps-resume/src/app/shared/util/transloco/providers.ts
+++ apps/ng-apps-resume/src/app/shared/util/transloco/providers.ts
@@ -1,14 +1,17 @@
-import {EnvironmentProviders, makeEnvironmentProviders} from '@angular/core';
+import {EnvironmentProviders, inject, makeEnvironmentProviders, provideAppInitializer} from '@angular/core';
 import {
     provideTransloco,
     provideTranslocoConfig,
     provideTranslocoLoadingTpl,
     provideTranslocoMissingHandler,
     TranslocoConfig,
+    TranslocoService,
 } from '@jsverse/transloco';
 import {provideTranslocoLocale} from '@jsverse/transloco-locale';
 import {MessageformatConfig} from '@jsverse/transloco-messageformat';
 import {provideTranslocoPersistLang} from '@jsverse/transloco-persist-lang';
+import {rxEffect} from 'ngxtension/rx-effect';
+import {filter, tap} from 'rxjs';
 import {Locale} from './locale';
 import {createPersistLangConfig, createTranslocoConfigWithDefault} from './transloco-config-factory';
 import {TranslocoErrorOnMissingHandler} from './transloco-error-on-missing-handler.service';
@@ -43,5 +46,17 @@ export function provideI18nUsingTransloco(options?: i18nWithTranslocoOptions): E
         //     ),
         // ),
         provideTranslocoPersistLang(createPersistLangConfig()),
+        provideAppInitializer(() => {
+            rxEffect(writeLang$());
+        }),
     ]);
+}
+
+function writeLang$() {
+    return inject(TranslocoService).langChanges$.pipe(
+        filter(Boolean),
+        tap((lang) => {
+            document.documentElement.lang = lang;
+        }),
+    );
 }
