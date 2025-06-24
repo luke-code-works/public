9eeceab59e318e42f80dc69a2cdf3cd6c756bb3b
Lukas Voland <lukas.voland@live.com>
2025-06-24 11:27:50 +0200

    fix: make umami initializer non-blocking

--- apps/ng-apps-resume/src/app/shared/util/umami/provider.ts
+++ apps/ng-apps-resume/src/app/shared/util/umami/provider.ts
@@ -1,7 +1,13 @@
 import {makeEnvironmentProviders, provideAppInitializer} from '@angular/core';
+import {rxEffect} from 'ngxtension/rx-effect';
 import {initializeUmami$} from './umami-init.functions';
 import {UmamiProxyService} from './umami-proxy.service';
 
 export function provideUmami() {
-    return makeEnvironmentProviders([provideAppInitializer(initializeUmami$), UmamiProxyService]);
+    return makeEnvironmentProviders([
+        provideAppInitializer(() => {
+            rxEffect(initializeUmami$());
+        }),
+        UmamiProxyService,
+    ]);
 }
