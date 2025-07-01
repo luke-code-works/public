4c280917edcc12a14ee7c964cd1150b2ce7b5dcc
Luke <133242630+luke-code-works@users.noreply.github.com>
2025-07-01 20:16:20 +0200

    feature/fix-github-actions (#68)
    * fix: repair broken github pipeline
    
    * chore: remove all tools cause of github actions
    
    * chore: remove migrations.json

--- tools/docker/build.ts
+++
@@ -1,9 +0,0 @@
-import {execSync} from 'child_process';
-
-export function buildDockerImage(file: string, tag: string, progress: 'auto' | 'plain' = 'auto') {
-    console.log('> docker image build');
-
-    execSync(`docker image build --file="${file}" --tag="${tag}" --progress="${progress}"  --no-cache .`, {
-        stdio: 'inherit',
-    });
-}
--- tools/docker/push.ts
+++
@@ -1,6 +0,0 @@
-import {execSync} from 'child_process';
-
-export function pushDockerImage(tag: string) {
-    console.log('> docker image push');
-    execSync(`docker image push "${tag}"`, {stdio: 'inherit'});
-}
