c91bbd3acf1299fef6e30049aee7ddf04414a92d
Lukas Voland <lukas.voland@live.com>
2025-07-01 20:14:44 +0200

    chore: remove all tools cause of github actions

--- tools/docker/save.ts
+++
@@ -1,9 +0,0 @@
-import {execSync} from 'child_process';
-
-export function saveDockerImage(file: string, tag: string) {
-    console.log('> docker image save');
-
-    execSync(`docker image save --output="${file}" "${tag}"`, {
-        stdio: 'inherit',
-    });
-}
